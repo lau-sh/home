7f450b06258c3454538217ed26abffe7701b5290
Eric Lau <elau89@gmail.com>
2017-01-22 11:49:10 -0500

    Added in automatic installer

---
+++ install.sh
@@ -0,0 +1,87 @@
+#!/bin/bash
+
+DISTRO=$(grep ^ID= /etc/*-release)
+DISTRO=${DISTRO##*=}
+
+CONVERSION_LIST=(
+ctags
+gitconfig
+gitignore
+oh-my-zsh
+profile
+tmux.conf
+vimrc
+zshrc
+)
+
+INSTALLATION_LIST=(
+curl
+git
+zsh
+tmux
+vim
+)
+
+case "$DISTRO" in
+    fedora)
+        INSTALL_CMD="sudo dnf install"
+        INSTALLATION_LIST="$INSTALLATION_LIST util-linux-user"
+        ;;
+    *)
+        echo "Unsupported distribution.  Please fix before using"
+        exit 1
+esac
+
+echo "Found distribution: $DISTRO"
+echo "Using \"$INSTALL_CMD\" as install command..."
+
+$INSTALL_CMD $INSTALLATION_LIST[*]
+
+if [ $? -ne 0 ]
+then
+    echo "Installation list failed.  Check the name of the files to be installed"
+    exit 2
+fi
+
+echo "Installing dein vim package manager..."
+
+curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > dein_installer.sh
+chmod +x dein_installer.sh
+
+mkdir -p $HOME/.vim/bundle
+sh dein_installer.sh $HOME/.vim/bundle
+
+if [ $? -ne 0 ]
+then
+    echo "Installing dein failed."
+    exit 3
+fi
+
+echo "Install oh-my-zsh..."
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+if [ $? -ne 0 ]
+then
+    echo "Installing oh-my-zsh failed."
+    exit 4
+fi
+
+echo "Installing user configuration files..."
+
+function moveToHome() {
+	cp -fr $1 $HOME/.$1
+}
+
+for file in "${CONVERSION_LIST[@]}"
+do
+	moveToHome $file
+done
+
+if [ $? -ne 0 ]
+then
+    echo "Moving configuration packages failed."
+    exit 5
+fi
+
+echo "Installation complete!"
